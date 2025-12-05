c8846344dc5907ee7c747dbe84fae84437f74c60
amunozf2021 <91094909+amunozf2021@users.noreply.github.com>
2025-12-05 23:09:04 +0100

    No links

--- app/layout.js
+++ app/layout.js
@@ -2,18 +2,16 @@ import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
-    title: "El Principito — Aventura Interactiva",
+  metadataBase: new URL("https://principito-dusky.vercel.app/"),
+  title: "El Principito — Aventura Interactiva",
+  description: "Una experiencia educativa e interactiva inspirada en El Principito."
 };
 
 export default function RootLayout({ children }) {
     return (
         <html lang="es" className="dark">
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <body>{children}</body>
-        <Analytics/>
+            <body>{children}</body>
+            <Analytics/>
         </html>
     );
 }
