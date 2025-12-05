df027b7c43e17984eee6f8bfbe2d4298f6221988
amunozf2021 <91094909+amunozf2021@users.noreply.github.com>
2025-12-05 22:06:50 +0100

    favicons

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="es" className="dark">
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <body>{children}</body>
         <Analytics/>
         </html>
